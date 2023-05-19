19a6b2b5367d5ee12546d275c81ef735262915ae
wjddh1999 <edong4255@gmail.com>
2023-05-19 23:10:38 +0900

    0519-3
    각도 변경 발판 추가

---
+++ My project (3)/Assets/Script/Switch.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigid;
+    public static bool isSwitch;
+    bool isPlayer = false;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) //Æ®¸®°Å¶û ´êÀº°æ¿ì
+        {
+            isPlayer = true;
+            Debug.Log("collision Switch");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer && Input.GetKeyDown(KeyCode.V))
+        {
+            if (isSwitch == false)
+                isSwitch = true;
+
+            else if (isSwitch == true)
+                isSwitch = false;
+        }
+
+        if (isSwitch == true)
+        {
+            animator.SetBool("isTrigger", true);
+            Debug.Log("Switch on");
+            RotateBar.isRotate = true;
+        }
+
+        else if (isSwitch == false)
+        {
+            animator.SetBool("isTrigger", false);
+            Debug.Log("Switch off");
+            RotateBar.isRotate = false;
+        }
+
+    }
+}
